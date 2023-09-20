20b64d80e522d86df8c06d78ff4e8eadf0db2291
Massimo <rabuffo.massimo@gmail.com>
2023-09-20 15:33:29 +0200

    esercizio corretto

--- exercise.js
+++ exercise.js
@@ -1,11 +1,9 @@
 const isLogged = true;
 
-//Ho risolto l'esercio inserndo le promise in due funzioni in modo tale da poter inserire i parametri nell esecuzione, ma non sono sicuro sia lo svolgimento corretto.
-
 function checkLogin(log) {
     return new Promise((resolve, reject) => {
         if(log) {
-            resolve (num = Math.random());
+            resolve (Math.random());
         } else reject(console.error('The variable is false'));
     })
 }
@@ -13,9 +11,9 @@ function checkLogin(log) {
 function checkNum(num) {
     return new Promise((resolve, reject) => {
         if(num > 0.5) {
-            resolve({name: "John", age: 24});
+            resolve(console.log({name: "John", age: 24}));
         } else reject(console.error('The number is less than 0.5.'));
     })
 }
 
-checkLogin(isLogged).then((num) => console.log(checkNum(num)));
+checkLogin(isLogged).then((num) => checkNum(num));
